fd90f3e1b22afb18f1ff625e98efb1c6f0cf4fcc
Manish Motwani <manishpmotwani@gmail.com>
2015-12-07 16:53:48 +0530

    added support for all email fields with mapping to mailgun fields.

--- Mailer/Transport/MailgunTransport.php
+++ Mailer/Transport/MailgunTransport.php
@@ -14,6 +14,33 @@ class MailgunTransport extends AbstractTransport
 {
 
 	/**
+	 * Email header to Mailgun param mapping
+	 *
+	 * @var array
+	 */
+	protected $_paramMapping = array(
+		'From' => 'from',
+		'Sender' => 'sender',
+		'Reply-To' => 'h:Reply-To',
+		'Disposition-Notification-To' => 'h:Disposition-Notification-To',
+		'Return-Path' => 'h:Return-Path',
+		'To' => 'to',
+		'Cc' => 'cc',
+		'Bcc' => 'bcc',
+		'Subject' => 'subject',
+		'o:tag' => 'o:tag',
+		'o:campaign' => 'o:campaign',
+		'o:deliverytime' => 'o:deliverytime',
+		'o:dkim' => 'o:dkim',
+		'o:testmode' => 'o:testmode',
+		'o:tracking' => 'o:tracking',
+		'o:tracking-clicks' => 'o:tracking-clicks',
+		'o:tracking-opens' => 'o:tracking-opens'
+	);
+
+	protected $_mailgunCustomDataPrefix = 'v:';
+
+	/**
 	 * Send email via Mailgun SDK
 	 *
 	 * @param Email $email
@@ -23,23 +50,32 @@ class MailgunTransport extends AbstractTransport
 	public function send(Email $email)
 	{
 		$config = $email->profile();
+		$email->domain($config['mailgun_domain']);
 
-		$headers = $email->getHeaders(['from', 'replyTo', 'to']);
-		$subject = $email->subject();
+		//'_headers' will include all extra tags that may be related to mailgun fields with prefix 'o:' or custom data with prefix 'v:'
+		$emailHeaders = ['from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'bcc', 'subject', '_headers'];
+		
+		$headers = $email->getHeaders($emailHeaders);
 
-		$message['html'] = $email->message(Email::MESSAGE_HTML);
-		$message['text'] = $email->message(Email::MESSAGE_TEXT);
+		foreach ($headers as $header => $value) {
+			if (isset($this->_paramMapping[$header]) && !empty($value)) {  //empty params are not excepted by mailgun, throws error
+				$key = $this->_paramMapping[$header];
+				$params[$key] = $value;
+				continue;
+			}
+			//Custom data must be in json format
+			if (strpos($header, $this->_mailgunCustomDataPrefix) === 0 && !empty($value)) {
+				$json = json_decode($value);
+				if (!is_null($json) && json_last_error() === JSON_ERROR_NONE) {
+					$params[$header] = $value;
+				}
+			}
+		}
+
+		$params['html'] = $email->message(Email::MESSAGE_HTML);
+		$params['text'] = $email->message(Email::MESSAGE_TEXT);
 
 		try {
-			$params = array(
-				'from' => $headers['From'],
-				'to' => $headers['To'],
-				'h:reply-to' => $headers['Reply-To'],
-				'subject' => $subject,
-				'text' => $message['text'],
-				'html' => $message['html']
-			);
-
 			$mailgun = new Mailgun\Mailgun($config['mailgun_api_key']);
 			$result = $mailgun->sendMessage($config['mailgun_domain'], $params);
 
